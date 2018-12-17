6b0166342e8b2bbbf1f647686725b777b32d8b8e
Erik Vardanyan <erikvardanyan@Eriks-MacBook-Pro.local>
2018-12-17 12:26:47 +0200

    try again

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+
 
 require("./routes/generalRoutes")(app);
 require("./routes/signUpRoutes")(app);
@@ -27,6 +25,10 @@ mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
 );
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
